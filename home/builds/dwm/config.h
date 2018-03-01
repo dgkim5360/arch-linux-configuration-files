cd10e37bf0eda4ea20c3faaf4bd91dc9930e652c
Don Kim <dgkimdev@gmail.com>
2018-03-01 21:03:33 +0900

    Add a command for clipmenu in dwm

--- home/builds/dwm/config.h
+++ home/builds/dwm/config.h
@@ -64,6 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *clipcmd[] = { "clipmenu", "-i", "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *backlightdowncmd[] = { "xbacklight", "-5%", NULL };
 static const char *backlightupcmd[] = { "xbacklight", "+5%", NULL };
@@ -73,6 +74,7 @@ static const char *soundupcmd[] = { "amixer", "-q", "sset", "Master", "5%+", NUL
 
 static Key keys[] = {
 	/* modifier           key											function        argument */
+	{ ShiftMask,          XK_Return,								spawn,          {.v = clipcmd } },
 	{ MODKEY,             XK_p,										spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,   XK_Return,							spawn,          {.v = termcmd } },
 	{ 0,									XF86MonBrightnessDown,	spawn,					{.v = backlightdowncmd } },
